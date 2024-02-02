ebf5e2e1e4ef3b9ce8a167b1e036b2ed1b97d8ca
RodolphoAra <rodolphoaramachado@gmail.com>
2024-02-02 19:56:03 -0300

    Calculadora

---
+++ 001.py
@@ -0,0 +1,28 @@
+print('---CALCULADORA EM PYTHON---')
+while True:
+    print('''Digite 1 para SOMA 
+Digite 2 para SUBTRAÇÃO 
+Digite 3 para MULTIPLICAÇÃO
+Digite 4 para DIVISÃO''')
+    p = int(input('Qual sua opção? '))
+    if 1 > p > 4:
+        print('Somente valores válidos.')
+    else:
+        n1 = float(input('Digite um nº: '))
+        n2 = float(input('Digite um nº: '))
+    if p == 1:
+        s = (n1 + n2)
+        print(f'A resposta é {s}')
+    elif p == 2:
+        sub = (n1 - n2)
+        print(f'A resposta é {sub}')
+    elif p == 3:
+        m = (n1 * n2)
+        print(f'A resposta é {m}')
+    else:
+        d = (n1 / n2)
+        print(f'A resposta é {d}')
+    fim = input('Deseja finalizar o programa (s/n)? ')
+    if fim == 's':
+        print('Até logo...')
+        break
