44cb63e1ce84868db237a9f2d1f15dd89b73b06b
RodolphoAra <rodolphoaramachado@gmail.com>
2024-02-17 12:50:16 -0300

    English version

--- 001.py
+++ 001.py
@@ -1,28 +1,28 @@
-print('---CALCULADORA EM PYTHON---')
+print('---Python Calculator---')
 while True:
-    print('''Digite 1 para SOMA 
-Digite 2 para SUBTRAÇÃO 
-Digite 3 para MULTIPLICAÇÃO
-Digite 4 para DIVISÃO''')
-    p = int(input('Qual sua opção? '))
-    if 1 > p > 4:
-        print('Somente valores válidos.')
+    print('''TYPE 1 for ADDITION 
+TYPE 2 for SUBTRACTION
+TYPE 3 for MULTIPLICATION
+TYPE 4 for DIVISION''')
+    p = int(input('Which option? '))
+    if p < 1 or p > 4:
+        print('Only valid values.')
     else:
-        n1 = float(input('Digite um nº: '))
-        n2 = float(input('Digite um nº: '))
+        n1 = float(input('Enter a number: '))
+        n2 = float(input('Enter a number: '))
     if p == 1:
         s = (n1 + n2)
-        print(f'A resposta é {s}')
+        print(f'The answer is {s}')
     elif p == 2:
         sub = (n1 - n2)
-        print(f'A resposta é {sub}')
+        print(f'The answer is {sub}')
     elif p == 3:
         m = (n1 * n2)
-        print(f'A resposta é {m}')
-    else:
+        print(f'The answer is {m}')
+    elif p == 4:
         d = (n1 / n2)
-        print(f'A resposta é {d}')
-    fim = input('Deseja finalizar o programa (s/n)? ').lower()
-    if fim == 's':
-        print('Até logo...')
+        print(f'The answer is {d}')
+    fim = input('Do you want to finish the program (y/n)? ').lower()
+    if fim == 'y':
+        print('See you soon...')
         break
